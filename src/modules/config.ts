be4ba2b626bf018160f451904a1dfff0b640bd26
Dinesh Kumar D <dinesh.kumar4@allen.in>
2025-08-19 15:21:10 +0530

    fix: url

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -2,12 +2,19 @@
 const ENV: string = process.env.NODE_ENV || 'development';
 
 const URLS: Record<string, string> = {
-  development: process.env.DEV_URL || 'https://console.allen-stage.in/',
+  development:
+    'https://allen-ic-stage-ui-live-web-pr-709-allen-frontend-team.vercel.app' ||
+    process.env.DEV_URL ||
+    'https://console.allen-stage.in/',
   stage:
+    'https://allen-ic-stage-ui-live-web-pr-709-allen-frontend-team.vercel.app' ||
     process.env.CUSTOM_URL ||
     process.env.STAGE_URL ||
     'https://console.allen-stage.in/',
-  production: process.env.PROD_URL || 'https://astra.allen.in/',
+  production:
+    'https://allen-ic-stage-ui-live-web-pr-709-allen-frontend-team.vercel.app' ||
+    process.env.PROD_URL ||
+    'https://astra.allen.in/',
 };
 
 const DEFAULT_URL: string = URLS[ENV] || URLS.development;
